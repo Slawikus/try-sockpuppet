a2cdfc91c28e2e94537172c47fed75e29a889e35
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-05-07 00:21:53 +0200

    Add some npm scripts and set staticfiles in settings

--- configuration/settings.py
+++ configuration/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# BASE_DIR = Path.cwd()
+STATICFILES_DIRS = [
+    ("js", f"{BASE_DIR}/dist/js"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
