9bf01db2c8b903fac17f2e4493a85fe6ebf2b780
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-05-07 00:26:57 +0200

    Add decrement to counter

--- todos/reflexes/testreflex_reflex.py
+++ todos/reflexes/testreflex_reflex.py
@@ -4,3 +4,6 @@ from sockpuppet.reflex import Reflex
 class TestreflexReflex(Reflex):
     def increment(self, step=1):
         self.count = int(self.element.dataset['count']) + step
+
+    def decrement(self, step=1):
+        self.count = int(self.element.dataset['count']) - step
