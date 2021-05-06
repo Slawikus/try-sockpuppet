1f3807891195ce5e2677f2c47d13ce2d281b234f
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-05-07 00:02:10 +0200

    Update settings to add channels via Redis and give asgi

--- configuration/settings.py
+++ configuration/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+ASGI_APPLICATION = 'sockpuppet.routing.application'
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -128,3 +131,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
