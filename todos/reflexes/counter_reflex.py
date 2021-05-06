79ad4cccc477683838fab2e577e0b2b865d304e7
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-05-07 01:13:07 +0200

    Add full throttle stimulus controller

--- todos/reflexes/counter_reflex.py
+++ todos/reflexes/counter_reflex.py
@@ -2,5 +2,5 @@ from sockpuppet import reflex
 
 
 class CounterReflex(reflex.Reflex):
-  def increment(self, step=1):
-    self.session['count'] = int(self.session['count']) + step
+    def increment(self, step=1):
+        self.session['count'] = int(self.session['count']) + step
