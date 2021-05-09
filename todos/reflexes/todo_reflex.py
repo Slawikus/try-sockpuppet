954d365cac7c857cc1b28d0bef042a121facafbf
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2021-05-09 12:49:51 +0200

    Cross page manipulation

--- todos/reflexes/todo_reflex.py
+++ todos/reflexes/todo_reflex.py
@@ -1,4 +1,5 @@
 from sockpuppet import reflex
+from sockpuppet.channel import Channel
 
 from todos.models import Todo
 
@@ -6,6 +7,15 @@ from todos.models import Todo
 class TodoReflex(reflex.Reflex):
     def add_todo(self, step=1):
         Todo.objects.create(description='NewReflexTest')
+        channel_id = self.get_channel_id()
+
+        channel = Channel(name=channel_id, identifier='{"channel":"StimulusReflex::Channel"}')
+        channel.morph({
+            'selector': '#todo-counter',
+            'html': 'hello',
+            'children_only': True,
+        })
+        channel.broadcast()
 
     def reload(self):
         pass
